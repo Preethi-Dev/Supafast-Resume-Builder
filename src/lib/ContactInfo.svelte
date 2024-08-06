5e8054c8db736531e81746a023fc1519b7846113
Preethi <selvampreethi360@gmail.com>
2024-08-06 23:56:34 +0530

    add Intro and ContactInfo component

---
+++ src/lib/ContactInfo.svelte
@@ -0,0 +1,33 @@
+<script>
+    import {completeUserInfo} from "../stores/store";
+    import {ICONS} from "../utils/constants"
+    export let contacts = $completeUserInfo.contacts;
+    
+    function handleBlur(e, key){
+        completeUserInfo.update(value => ({...value, contacts: {...value.contacts, [key]: e.target.innerText}}));
+    }
+</script>
+<div class="container">
+    {#each Object.keys(contacts) as key}   
+        <div class="contact"><img src={ICONS[key]} alt="icon"><p bind:innerText={contacts[key]} on:blur={(e) => handleBlur(e, key)} contenteditable>{contacts[key]}</p></div>
+    {/each}
+</div>
+
+<style>
+    p{
+        color: rgba(0,0,0,1);
+        font-size: 1.125rem;
+        max-width: 38rem;
+    }
+
+    .container{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .contact{
+        display: flex;
+        align-items: center;
+        gap: .75rem;
+    }
+</style>
---
+++ src/lib/Intro.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Avatar from "./Atoms/Avatar.svelte";
+  import Bio from "./Atoms/Bio.svelte";
+  import Designation from "./Atoms/Designation.svelte";
+  import Name from "./Atoms/Name.svelte";
+</script>
+
+<div class="intro">
+    <div class="intro__container">
+        <div>
+            <Name />
+            <Designation />
+        </div>
+        <Bio />
+    </div>
+    <Avatar />
+</div>
+
+
+<style>
+    .intro{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .intro__container{
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+        max-width: 38rem;
+    }
+</style>
