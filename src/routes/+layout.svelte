4eafb57b2e55f9d0683c0fd6c4f7c9258a7bb10c
Preethi <selvampreethi360@gmail.com>
2024-08-06 16:29:12 +0530

    tweak bg based on URL

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
+<script>
+    import "../styles/global.css"; 
+    import { page } from '$app/stores';
+    import NavBar from "$lib/NavBar.svelte";
+
+    let isHome;
+    $: isHome = $page.route.id === "/";
+    $: isEditPage = $page.route.id.includes("edit");
+    
+</script>
+
+{#if isEditPage}
+    <NavBar />
+{/if}
 <main>
-    <div class="container">
+    <div class="container {isHome ? "bg" : ""} {isEditPage ? "" : "main-center"}">
         <slot></slot>
     </div>
 </main>
@@ -7,23 +21,32 @@
 <style>
     main{
         display: flex;
-        align-items: center;
         min-height: 100vh;
         padding: 4.1875rem 3.8125rem;
+    }
+
+    .main-center{
+        align-items: center;
     }
 
     .container{
         flex-grow: 1;
         border-radius: 2rem;
         background: rgba(255, 255, 255, 0.70);
+        padding: 3rem 4rem;
 
         box-shadow: 0px -44px 92px 0px rgba(255, 255, 255, 0.90), 0px 20px 36px 0px rgba(255, 255, 255, 0.75);
-        backdrop-filter: blur(2px);
+        backdrop-filter: blur(2px); 
+    }
+
+    .bg{
         background-image: url('/bg.svg');
         background-size: cover; 
         background-position: center; 
         background-repeat: no-repeat; 
         width: 100%; 
-        height: 100%; 
+        height: 100%;
     }
+
+
 </style>
