282038f54f0fbe36ee0a8eda1fbf7afc3ddec292
Preethi <selvampreethi360@gmail.com>
2024-08-06 23:58:57 +0530

    build resume's intro part

--- src/routes/template/edit/+page.js
+++ src/routes/template/edit/+page.js
@@ -1,13 +1,28 @@
-// src/routes/template/edit/+page.js
 import { redirect } from "@sveltejs/kit";
 import { get } from "svelte/store";
-import { userInfo } from "../../../stores/store";
+import { userInfo, completeUserInfo } from "../../../stores/store";
 
 export async function load() {
   const user = get(userInfo);
 
   if (!user) {
     throw redirect(302, "/"); // Redirect to home if userInfo is empty
+  } else {
+    completeUserInfo.update((value) => ({
+      ...value,
+      name: user.name || user.login,
+      designation: user.designation || "Your Designation",
+      bio:
+        user.bio ||
+        "Tell about yourself like (eg: Passionate about creating complex layouts and web components. Enjoy the challenge of continuously refining and improving designs. Technologies I commonly use: HTML, CSS3, JavaScript (async/await), React, Next.js, Redux and even more)",
+      avatar: user.avatar_url,
+      contacts: {
+        phone: "+91-xxxxxxxxxx",
+        email: user.email || "name@domain.com",
+        links: user.html_url,
+        location: user.location,
+      },
+    }));
   }
 
   // If you need to return data, do it here
---
+++ src/routes/template/edit/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Intro from "$lib/Intro.svelte";
+    import ContactInfo from "$lib/ContactInfo.svelte";
+    import {userInfo, currentTemplate, completeUserInfo} from "../../../stores/store";
+
+    $: console.log($completeUserInfo);
+</script>
+
+<div class="resume">
+    <div class="intro">
+        <Intro />
+        <ContactInfo />
+    </div>
+</div>
+
+<style>
+    .resume{
+        background: #F8F8F8;
+    }
+    
+    .intro{
+        display: flex;
+        flex-direction: column;
+        gap: 4.5rem;
+        padding: 2rem 3rem;
+        border-radius: 2rem;
+        background-color: #fff;
+    }
+</style>
