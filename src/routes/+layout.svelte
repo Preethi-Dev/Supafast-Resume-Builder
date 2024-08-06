1c9b8ddaf7217be6ab671fb2b50d686031ade7b1
Preethi <selvampreethi360@gmail.com>
2024-08-06 23:58:12 +0530

    tweak spacing based on URL

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
     <NavBar />
 {/if}
 <main>
-    <div class="container {isHome ? "bg" : ""} {isEditPage ? "" : "main-center"}">
+    <div class="container {isHome ? "bg" : ""} {isEditPage ? "shrink-padding" : "main-center grow-padding"}">
         <slot></slot>
     </div>
 </main>
@@ -25,6 +25,14 @@
         padding: 4.1875rem 3.8125rem;
     }
 
+    .shrink-padding{
+        padding: 1.5rem;
+    }
+
+    .grow-padding{
+        padding: 3rem 4rem;
+    }
+
     .main-center{
         align-items: center;
     }
@@ -33,7 +41,6 @@
         flex-grow: 1;
         border-radius: 2rem;
         background: rgba(255, 255, 255, 0.70);
-        padding: 3rem 4rem;
 
         box-shadow: 0px -44px 92px 0px rgba(255, 255, 255, 0.90), 0px 20px 36px 0px rgba(255, 255, 255, 0.75);
         backdrop-filter: blur(2px); 
