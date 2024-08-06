016b91eaef69a7e97daf83797c41b6da1702a6a2
Preethi <selvampreethi360@gmail.com>
2024-08-06 16:29:33 +0530

    add NavBar component

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<div class="nav">
+    <a href="/"><img src="/logo-only.png" alt=""></a>
+    <div class="nav__btn">
+        <img src="/preview.svg" alt="preview div" />
+        <a href="/template/preview">Preview</a>
+    </div>
+</div>
+
+<style>
+    .nav{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        border-radius: 6rem;
+        background: rgba(255, 255, 255, 0.40);
+        backdrop-filter: blur(12px);
+        padding: 1.125rem 2.1875rem;
+        margin: 1.5rem;
+    }
+
+    .nav__btn{
+        display: flex;
+        padding: 0.75rem 1rem;
+        align-items: center;
+        gap: 0.25rem;
+    }
+
+    .nav__btn > a{
+        color: #2C0A70;
+        font-size: 1rem;
+    }
+
+</style>
