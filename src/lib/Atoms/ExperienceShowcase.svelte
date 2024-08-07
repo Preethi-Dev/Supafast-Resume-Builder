afb6402c2c05f9ab1ba163d380dae4e50ea942fa
Preethi <selvampreethi360@gmail.com>
2024-08-07 13:07:50 +0530

    add experience component

---
+++ src/lib/Atoms/ExperienceShowcase.svelte
@@ -0,0 +1,64 @@
+<script>
+    import {completeUserInfo} from "../../stores/store";
+    let year = "Year";
+    let designation = "Your Designation...";
+    let companyName = "company name...";
+    let description = "describe your experience milestones...";
+
+    function handleBlur(e, key, value){
+        completeUserInfo.update(obj => ({...obj, experience: {...obj.experience, [key]: value}}))
+        console.log($completeUserInfo.experience);
+        
+    }
+</script>
+
+<div class="project">
+    <div class="project__intro">
+        <p bind:innerText={year} class="project__time" contenteditable on:blur={(e) => handleBlur(e, "year", year)}></p>
+        <p bind:innerText={companyName} class="project__designation" contenteditable on:blur={(e) => handleBlur(e, "company_name", companyName)}></p>
+    </div>
+    
+    <div class="project__info">
+        <p bind:innerText={designation} class="project__name" contenteditable on:blur={(e) => handleBlur(e, "designation", designation)}></p>
+        <p bind:innerText={description} class="project__description" contenteditable on:blur={(e) => handleBlur(e, "description", description)}></p>
+    </div>
+</div>
+
+<style>
+    .project{
+        display: flex;
+        gap: 4.5rem;
+        max-width: 52rem;
+        width: 52rem;
+    }
+
+    .project__intro{
+         max-width: 4.5rem;
+    }
+
+    .project__time, .project__Designation{
+        width: 4.5rem;
+        align-self: flex-end;
+        font-size: 1rem;
+        opacity: .5;
+    }
+
+    .project__Designation{
+        font-size: 1rem;
+    }
+
+    .project__info{
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+    }
+
+    .project__name{
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+
+    .project__description{
+        font-size: 1.125rem;
+    }
+</style>
---
+++ src/lib/Experience.svelte
@@ -0,0 +1,93 @@
+<script>
+  import ExperienceShowcase from "./Atoms/ExperienceShowcase.svelte";
+
+  let isEditMode = false;
+  let isDeleted = false;
+
+  function handleMouseEnter(e){
+    isEditMode = true; 
+  }
+  function handleMouseLeave(e){
+    isEditMode = false;
+  }
+  function handleDelete(e){
+    isDeleted = true;
+  }
+</script>
+
+{#if !isDeleted}
+  <div class="experience" on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave}>
+    {#if isEditMode}
+    <div class="experience__edit">
+        <img src="/delete.svg" alt="" on:click={handleDelete}>
+    </div>
+    {/if}
+    <div class="experience__title">
+      <p>Experience</p>
+      <div class="horizontal-line"></div>
+    </div>
+    <div class="experience__showcase">
+        <ExperienceShowcase />
+    </div>
+  </div>
+{/if}
+
+<style>
+  .experience {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    gap: 1.5rem;
+    padding: .5rem;
+    position: relative;
+  }
+
+  .experience:hover {
+    border-radius: 0.5rem;
+    border: 1px solid #A983F4;
+  }
+
+  .experience__title {
+    display: flex;
+    align-items: center;
+    gap: 2.5rem;
+  }
+
+  .experience__edit {
+    position: absolute;
+    right: 0;
+    top: -1.5rem;
+    display: inline-flex;
+    gap: 1rem;
+  }
+
+  .experience__edit > * {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    padding: .5rem;
+    background: #FFF;
+    box-shadow: 0px 4px 36px 0px rgba(169, 131, 244, 0.06);
+    backdrop-filter: blur(6px);
+  }
+
+  .experience__showcase {
+    align-self: flex-end;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+
+  p {
+    color: #000;
+    font-size: 2.5rem;
+  }
+
+  .horizontal-line {
+    height: 0.125rem;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.12);
+  }
+</style>
