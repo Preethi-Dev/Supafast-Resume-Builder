1e9a9e161705c89d4995affa4a88e46776323fd4
Preethi <selvampreethi360@gmail.com>
2024-08-07 13:07:34 +0530

    add education component

---
+++ src/lib/Atoms/EducationShowcase.svelte
@@ -0,0 +1,54 @@
+<script>
+    import {completeUserInfo} from "../../stores/store";
+    let year = "Year";
+    let name = "Course Name";
+    let description = "describe your academic milestones...";
+
+    function handleBlur(e, key, value){
+        completeUserInfo.update(obj => ({...obj, education: {...obj.education, [key]: value}}))
+        console.log($completeUserInfo.education);
+        
+    }
+</script>
+
+<div class="project">
+    
+    <p bind:innerText={year} class="project__time" contenteditable on:blur={(e) => handleBlur(e, "year", year)}></p>
+    <div class="project__info">
+        <p bind:innerText={name} class="project__name" contenteditable on:blur={(e) => handleBlur(e, "name", name)}></p>
+        <p bind:innerText={description} class="project__description" contenteditable on:blur={(e) => handleBlur(e, "description", description)}></p>
+    </div>
+</div>
+
+<style>
+    .project{
+        display: flex;
+        gap: 4.5rem;
+        max-width: 52rem;
+        width: 52rem;
+    }
+
+    .project__time{
+        width: 4.5rem;
+        align-self: flex-end;
+        font-size: 1rem;
+        opacity: .5;
+        flex-shrink: 0;
+        align-self: flex-start;
+    }
+
+    .project__info{
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+    }
+
+    .project__name{
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+
+    .project__description{
+        font-size: 1.125rem;
+    }
+</style>
---
+++ src/lib/Education.svelte
@@ -0,0 +1,95 @@
+<script>
+  import EducationShowcase from "./Atoms/EducationShowcase.svelte";
+
+  
+  let isEditMode = false;
+  let isDeleted = false;
+  
+
+  function handleMouseEnter(e){
+    isEditMode = true; 
+  }
+  function handleMouseLeave(e){
+    isEditMode = false;
+  }
+  function handleDelete(e){
+    isDeleted = true;
+  }
+</script>
+
+{#if !isDeleted}
+  <div class="education" on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave}>
+    {#if isEditMode}
+    <div class="education__edit">
+        <img src="/delete.svg" alt="" on:click={handleDelete}>
+    </div>
+    {/if}
+    <div class="education__title">
+      <p>Eductaion</p>
+      <div class="horizontal-line"></div>
+    </div>
+    <div class="education__showcase">
+        <EducationShowcase />
+    </div>
+  </div>
+{/if}
+
+<style>
+  .education {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    gap: 1.5rem;
+    padding: .5rem;
+    position: relative;
+  }
+
+  .education:hover {
+    border-radius: 0.5rem;
+    border: 1px solid #A983F4;
+  }
+
+  .education__title {
+    display: flex;
+    align-items: center;
+    gap: 2.5rem;
+  }
+
+  .education__edit {
+    position: absolute;
+    right: 0;
+    top: -1.5rem;
+    display: inline-flex;
+    gap: 1rem;
+  }
+
+  .education__edit > * {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    padding: .5rem;
+    background: #FFF;
+    box-shadow: 0px 4px 36px 0px rgba(169, 131, 244, 0.06);
+    backdrop-filter: blur(6px);
+  }
+
+  .education__showcase {
+    align-self: flex-end;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+
+  p {
+    color: #000;
+    font-size: 2.5rem;
+  }
+
+  .horizontal-line {
+    height: 0.125rem;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.12);
+  }
+</style>
