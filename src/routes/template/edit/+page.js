cd7800fd7cd7fb676caa3d07d1a1ef8f64fa36c9
Preethi <selvampreethi360@gmail.com>
2024-08-07 23:09:25 +0530

    make restrictions while fetch data

--- src/routes/template/edit/+page.js
+++ src/routes/template/edit/+page.js
@@ -14,26 +14,29 @@ export async function load({ fetch }) {
     throw redirect(302, "/"); // Redirect to home if userInfo is empty
   } else {
     //fetch repositories
-    const response = await fetch(user.repos_url);
-    const data = await response.json();
-    repos.set(data);
-    currentRepos.set(data.filter((repo, index) => index < 3));
+    if (Object.keys(get(completeUserInfo)).length === 0) {
+      const response = await fetch(user.repos_url);
+      const data = await response.json();
+      repos.set(data);
 
-    completeUserInfo.update((value) => ({
-      ...value,
-      name: user.name || user.login,
-      designation: user.designation || "Your Designation",
-      bio:
-        user.bio ||
-        "Tell about yourself like (eg: Passionate about creating complex layouts and web components. Enjoy the challenge of continuously refining and improving designs. Technologies I commonly use: HTML, CSS3, JavaScript (async/await), React, Next.js, Redux and even more)",
-      avatar: user.avatar_url,
-      contacts: {
-        phone: "+91-xxxxxxxxxx",
-        email: user.email || "name@domain.com",
-        links: user.html_url,
-        location: user.location || "your location",
-      },
-    }));
+      currentRepos.set(data.filter((repo, index) => index < 3));
+
+      completeUserInfo.update((value) => ({
+        ...value,
+        name: user.name || user.login,
+        designation: user.designation || "Your Designation",
+        bio:
+          user.bio ||
+          "Tell about yourself like (eg: Passionate about creating complex layouts and web components. Enjoy the challenge of continuously refining and improving designs. Technologies I commonly use: HTML, CSS3, JavaScript (async/await), React, Next.js, Redux and even more)",
+        avatar: user.avatar_url,
+        contacts: {
+          phone: "+91-xxxxxxxxxx",
+          email: user.email || "name@domain.com",
+          links: user.html_url,
+          location: user.location || "your location",
+        },
+      }));
+    }
   }
 
   // If you need to return data, do it here
